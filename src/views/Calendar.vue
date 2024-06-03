386c91415e700450107cadb2244a7225942ae11e
moushow <1060047803@qq.com>
2024-06-03 18:44:33 +0800

    日历视图动态绑定

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
     <el-calendar>
-      <template
-          slot="dateCell"
-          slot-scope="{date, data}">
+      <template slot="dateCell" slot-scope="{ date, data }">
         <div
             style="height: 100%; position: relative;"
             :class="['custom-date-cell', data.isSelected ? 'is-selected' : '']"
-            @click="handleClick(data.day)">
-          <div class="date">{{ data.day.split('-').slice(1).join('-') }}</div>
+            @click="handleClick(data.day)"
+        >
+          <div class="date">{{ getNextDay(data.day).split('-').slice(1).join('-') }}</div>
           <div class="content" v-if="getEventForDate(data.day)">
             {{ getEventForDate(data.day).event }}
           </div>
@@ -24,7 +23,8 @@ export default {
   data() {
     return {
       value: new Date(),
-      arr: []
+      arr: [],
+      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
     };
   },
   created() {
@@ -32,33 +32,51 @@ export default {
   },
   methods: {
     handleClick(date) {
-      alert(date);
+      const event = this.getEventForDate(date);
+      if (event) {
+        this.showMessage(event.event);
+      } else {
+        this.showMessage('暂无事务');
+      }
     },
     load() {
-      fetch("http://localhost:9090/schedule")
-          .then(res => res.json())
-          .then(res => {
+      const username = localStorage.getItem('username');
+      fetch(`http://localhost:9090/schedule/${this.user.username}`)
+          .then((res) => res.json())
+          .then((res) => {
             if (res && Array.isArray(res)) {
-              this.arr = res.map(item => ({
+              this.arr = res.map((item) => ({
                 date: item.date,
-                event: item.event
+                event: item.event,
               }));
             }
           })
-          .catch(error => {
+          .catch((error) => {
             console.error("Error fetching schedule data:", error);
           });
     },
     getEventForDate(date) {
-      return this.arr.find(v => v.date === date);
-    }
-  }
+      return this.arr.find((v) => v.date === date);
+    },
+    getNextDay(date) {
+      const nextDay = new Date(date);
+      nextDay.setDate(nextDay.getDate() + 1);
+      return nextDay.toISOString().split('T')[0];
+    },
+    showMessage(message) {
+      this.$message({
+        message: message,
+        type: 'info',
+        customClass: 'custom-message',
+      });
+    },
+  },
 };
 </script>
 
 <style scoped>
 .custom-date-cell {
-  border: 2px solid transparent;
+  border: 4px solid transparent;
   transition: border-color 0.3s ease;
   display: flex;
   flex-direction: column;
@@ -92,5 +110,9 @@ export default {
   color: #666;
   margin-top: 20px;
   text-align: center;
+}
+
+.custom-message .el-message__content {
+  color: #1890ff !important;
 }
 </style>
