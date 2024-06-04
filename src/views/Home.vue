981493a19d6104a5f89a7a50040f2f728ec4c35c
Emma Thompson <1245989664@qq.com>
2024-06-04 15:58:06 +0800

    首页折线图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div id="main" style="width: 500px; height: 400px;"></div>
+    <div id="main" style="width: 1000px; height: 400px"></div>
   </div>
 </template>
 
@@ -10,34 +10,48 @@ import * as echarts from 'echarts';
 export default {
   name: "Home",
   data() {
-    return {};
+    return {
+      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
+    }
   },
   mounted() {
-    var chartDom = document.getElementById('main');
-    var myChart = echarts.init(chartDom);
-    var option = {
+  
+
+    var option = {  
+      
       xAxis: {
         type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        data: []
       },
       yAxis: {
         type: 'value'
       },
       series: [
         {
-          data: [150, 230, 224, 218, 135, 147, 260],
+          data: [],
           type: 'line'
+        },
+        {
+          data: [],
+          type: 'bar'
         }
       ]
     };
-    myChart.setOption(option);
+    var chartDom = document.getElementById('main');
+    var myChart = echarts.init(chartDom);
+
+    const username = this.user.username
+
+    this.request.get("/echarts/example/" + username).then(res => {
+      console.log(res)
+      option.xAxis.data = res.data.x;
+      option.series[0].data = res.data.y;
+      option.series[1].data = res.data.y;
+      myChart.setOption(option);
+    })
   }
 }
 </script>
 
 <style scoped>
-#main {
-  width: 500px;
-  height: 400px;
-}
 </style>
