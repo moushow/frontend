f10e2e0dee0e9b57e7a3fb272909f8fc3c2153c6
moushow <1060047803@qq.com>
2024-06-07 21:13:47 +0800

    右上显示头像

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="line-height: 60px; display: flex; justify-content: space-between; align-items: center;">
+  <div style="line-height: 60px; display: flex; justify-content: space-between; align-items: center; width: auto">
     <div style="display: flex; align-items: center;">
       <span :class="collapseBtnClass" style="cursor: pointer; font-size: 20px;" @click="collapse"></span>
       <el-breadcrumb separator="/" style="display:inline-block; margin-left: 10px">
@@ -7,9 +7,12 @@
         <el-breadcrumb-item>{{ currentPathName }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
-    <el-dropdown style="cursor: pointer; display: flex; align-items: center;">
-      <span>{{ user.nickname }}</span>
-      <i class="el-icon-arrow-down" style="margin-left: 5px;"></i>
+    <el-dropdown style="width: 150px; cursor: pointer; text-align: right">
+      <div style="display: inline-block; position: relative; z-index: 1;">
+        <img :src="user.avatar" alt="" style="width: 30px; border-radius: 50%; position: relative; top: 10px; right: 5px">
+        <span style="position: relative; z-index: 2;">{{ user.nickname }}</span>
+        <i class="el-icon-arrow-down" style="margin-left: 10px;"></i>
+      </div>
       <el-dropdown-menu slot="dropdown">
         <el-dropdown-item style="font-size: 14px; padding: 5px 0">
           <router-link to="/person" style="text-decoration: none">个人信息</router-link>
